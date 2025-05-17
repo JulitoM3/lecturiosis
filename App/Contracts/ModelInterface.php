b15a977705e5c0395eaba65be0f074fefea845c4
Julio Guerrero <juliocaramelo44@gmail.com>
2025-05-17 10:29:39 -0600

    first

---
+++ App/Contracts/ModelInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Contracts;
+
+interface ModelInterface
+{
+
+    public function save();
+
+    public function delete();
+    public function findById($id);
+}
---
+++ App/Models/Model.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Models;
+
+use App\Contracts\ModelInterface;
+
+class Model implements ModelInterface
+{
+
+    protected static $table = '';
+
+
+    public function save()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function findById($id)
+    {
+
+    }
+
+}
